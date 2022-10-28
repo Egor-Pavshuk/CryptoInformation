0014f730d58b71606a48da9158ed539edb578db9
Егор Павшук <egor.pavshuk@outlook.com>
2022-10-28 23:46:39 +0300

    Start development

---
+++ CryptoBll/Services/CryptoServices.cs
@@ -0,0 +1,18 @@
+﻿using CryptoInterface.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoBll.Services
+{
+    internal class CryptoServices
+    {
+        public async Task<Currency> GetFamousCurrencies()
+        {
+            return null;
+        }
+
+    }
+}
