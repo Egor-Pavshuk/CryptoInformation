a389265ea1476efdbd6b310679d144e9c74ce6ac
Егор Павшук <egor.pavshuk@outlook.com>
2022-10-28 23:31:36 +0300

    Add project files.

---
+++ CryptoBll/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace CryptoBll
+{
+    public class Class1
+    {
+    }
+}
---
+++ CryptoInterface/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace CryptoInterface
+{
+    public class Class1
+    {
+    }
+}
